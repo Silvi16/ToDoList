5efa3d69cc5f395f0c0d252a3f6c2b97ee2c658a
Silvia Krajnakova <silvia.krajnakova@gmail.com>
2021-05-27 22:26:37 +0200

    Add pages about and work, partials, date.js with exports

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
+const date = require('./date.js')
 
 const app = express()
 
@@ -7,43 +8,43 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-let newItems = ['Buy food','Cook food', 'Eat food']
-let workItems = []
+const newItems = ['Buy food','Cook food', 'Eat food']
+const workItems = []
 
-app.get('/', (req, res) => {
-    let today = new Date()
-
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
-    let day = today.toLocaleDateString('en-US', options)
-
+app.get('/home', (req, res) => {
+    let day = date.getDate()
     res.render('list', {
         listTitle: day,
-        newListItems: newItems
+        newListItems: newItems,
+        title: 'home'
     })
 })
 
 app.get('/work', (req, res) => {
-    res.render('list', {
-        listTitle: 'Work List',
-        newListItems: workItems
+    let day = date.getDay()
+    res.render('work', {
+    listTitle: day,
+    newWorkItems: workItems,
+    title: 'work'
     })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        listTitle: 'About',
+        title: 'about'})
+})
 
 app.post('/', (req, res) => {
-    if (req.body.list === 'Work') {
-        let workItem = req.body.newItem
-        workItems.push(workItem)
-        res.redirect('/work')
-    } else {
-        let newItem = req.body.newItem
-        newItems.push(newItem)
-        res.redirect('/')
-    }
+    let newItem = req.body.newItem
+    newItems.push(newItem)
+    res.redirect('/')
+})
+
+app.post('/work', (req, res) => {
+    let workItem = req.body.newItem
+    workItems.push(workItem)
+    res.redirect('/work')
 })
 
 
